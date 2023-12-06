70612e7b47d802faeed577cbd58ac9377380620a
Ben Cohen <ben_cohen@apple.com>
2023-12-05 20:04:44 -0800

    Add a --all command line option to run all days

--- Sources/AdventOfCode.swift
+++ Sources/AdventOfCode.swift
@@ -13,6 +13,9 @@ struct AdventOfCode: AsyncParsableCommand {
   @Flag(help: "Benchmark the time taken by the solution")
   var benchmark: Bool = false
 
+  @Flag(help: "Run all the days available")
+  var all: Bool = false
+
   /// The selected day, or the latest day if no selection is provided.
   var selectedChallenge: any AdventDay {
     get throws {
@@ -52,17 +55,25 @@ struct AdventOfCode: AsyncParsableCommand {
   }
 
   func run() async throws {
-    let challenge = try selectedChallenge
-    print("Executing Advent of Code challenge \(challenge.day)...")
+    let challenges =
+      if all {
+        allChallenges
+      } else {
+        try [selectedChallenge]
+      }
 
-    let timing1 = await run(part: challenge.part1, named: "Part 1")
-    let timing2 = await run(part: challenge.part2, named: "Part 2")
+    for challenge in challenges {
+      print("Executing Advent of Code challenge \(challenge.day)...")
 
-    if benchmark {
-      print("Part 1 took \(timing1), part 2 took \(timing2).")
-      #if DEBUG
-        print("Looks like you're benchmarking debug code. Try swift run -c release")
-      #endif
+      let timing1 = await run(part: challenge.part1, named: "Part 1")
+      let timing2 = await run(part: challenge.part2, named: "Part 2")
+
+      if benchmark {
+        print("Part 1 took \(timing1), part 2 took \(timing2).")
+        #if DEBUG
+          print("Looks like you're benchmarking debug code. Try swift run -c release")
+        #endif
+      }
     }
   }
 }
